ddf077d1316f97eda322780bdc6e24944de29c87
akashr206 <akashr6514@gmail.com>
2025-08-08 18:41:15 +0530

    integrate database

---
+++ models/user.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        id: { type: String, required: true },
+        profile: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
